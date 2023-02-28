ac93e3a69739b40444e4ebf026f29ac1ebc98fd3
toluhunter <toluhunter19@gmail.com>
2023-02-28 17:35:26 +0100

    Added Upload Project Functionality

--- repository/models.py
+++ repository/models.py
@@ -11,20 +11,24 @@ from account.models import Institution
 
 User = get_user_model()
 
+
 def set_folder_name(model):
     title = model.id
-    folder = os.path.join("repo", title)
-        
+    folder = os.path.join("repo", model.project_id)
+
     return folder
+
 
 def set_cover_upload(model, *args, **kwargs):
     folder = set_folder_name(model)
     return os.path.join(folder, "coverpage")
 
+
 def set_document_upload(model, filename):
     folder = set_folder_name(model)
 
     return os.path.join(folder, "project")
+
 
 def year_validator(value):
     minimum_year = datetime.now().year - 20
@@ -32,12 +36,12 @@ def year_validator(value):
 
     if not re.match(r"\d{4}", value):
         raise ValidationError("Not a valid Year")
-    
+
     if int(value) < minimum_year and int(value) > maximum_year:
         raise ValidationError("Resource too old")
 
     return value
-        
+
 
 class Department(models.Model):
 
@@ -50,44 +54,53 @@ class Department(models.Model):
     def save(self, *args, **kwargs):
         self.full_clean()
         return super().save(*args, **kwargs)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
+
+
 class Project(models.Model):
 
     def set_id():
         return randint(int("1"*15), int("9"*15))
 
     id = models.IntegerField(primary_key=True, default=set_id)
-    project_id = models.CharField(max_length=115, null=False, blank=False, unique=True)
-    title = models.CharField(max_length=100, unique=True, null=False, blank=False)
-    institution = models.ForeignKey(to=Institution, on_delete=models.CASCADE, related_name="projects", null=False, blank=False)
-    department = models.ForeignKey(to=Department, on_delete=models.CASCADE, null=False, blank=False, related_name="projects")
+    project_id = models.CharField(
+        max_length=115, null=False, blank=False, unique=True)
+    title = models.CharField(
+        max_length=100, unique=True, null=False, blank=False)
+    institution = models.ForeignKey(
+        to=Institution, on_delete=models.CASCADE, related_name="projects", null=False, blank=False)
+    department = models.ForeignKey(
+        to=Department, on_delete=models.CASCADE, null=False, blank=False, related_name="projects")
     scholar = models.ForeignKey(to=User, on_delete=models.SET_NULL, null=True)
     description = models.TextField(null=False, blank=False)
     cover_page = models.ImageField(upload_to=set_cover_upload, null=True)
     document = models.FileField(upload_to=set_document_upload)
     url = models.URLField(null=True)
     supervisor = models.CharField(max_length=50, null=False, blank=False)
-    date_uploaded = models.DateTimeField(auto_now_add=True, null=False, blank=False)
-    year_published = models.CharField(max_length=4,validators=[year_validator], null=False, blank=False)
+    date_uploaded = models.DateTimeField(
+        auto_now_add=True, null=False, blank=False)
+    year_published = models.CharField(max_length=4, validators=[
+                                      year_validator], null=False, blank=False)
     views = models.PositiveIntegerField(default=0, null=False, blank=False)
     plagiarism_score = models.FloatField(null=False, blank=False)
+    tags = models.ManyToManyField(to=Tag, related_name="projects")
 
     def set_project_id(self):
         title = self.title.replace(" ", "-").lower()
         return f"{title}-{self.id}"
-            
+
     def __str__(self):
         return self.title
 
     def save(self, **kwargs):
         if not self.project_id:
-            self.id = self.set_project_id()
+            self.project_id = self.set_project_id()
 
         self.full_clean()
         return super().save(**kwargs)
-
-class Tag(models.Model):
-    name = models.CharField(max_length=30)
-    projects = models.ManyToManyField(to=Project, related_name="tags")
-
-    def __str__(self):
-        return self.name
--- repository/views.py
+++ repository/views.py
@@ -5,8 +5,15 @@ from django.urls import reverse_lazy
 
 from .forms import ProjectForm
 
-class CreateProjectView(LoginRequiredMixin, generic.FormView):
+
+class CreateProjectView(LoginRequiredMixin, generic.CreateView):
 
     form_class = ProjectForm
     template_name = 'createproject.html'
     success_url = reverse_lazy("home")
+
+    def get_form_kwargs(self):
+        kwargs = super().get_form_kwargs()
+        kwargs["request"] = self.request
+
+        return kwargs
