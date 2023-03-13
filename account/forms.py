02c782464623226a38f6d7981d7bf6a4588937c1
toluhunter <toluhunter19@gmail.com>
2023-03-13 22:19:00 +0100

    Added account update functionality

--- account/forms.py
+++ account/forms.py
@@ -77,7 +77,7 @@ class AccountCreationForm(forms.ModelForm):
         return user
 
 
-class AccountUpdateForm(forms.ModelForm):
+class ChangePasswordForm(forms.ModelForm):
 
     old_password = forms.CharField(
         required=True,
@@ -101,14 +101,64 @@ class AccountUpdateForm(forms.ModelForm):
     class Meta:
         model = User
         fields = [
-            "email",
             "old_password",
             "password",
             "c_password",
+        ]
+
+    def __init__(self, request=None, *args, **kwargs) -> None:
+        self.request = request
+
+        super().__init__(*args, **kwargs)
+
+        self.fields["password"].widget = forms.PasswordInput(
+            attrs={
+                "class": "w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:border-indigo-500 focus:outline-none focus:shadow-outline",
+                "placeholder": "*"*20
+            }
+        )
+
+    def clean(self):
+        old_password = self.cleaned_data.pop("old_password")
+        if not self.request.user.check_password(old_password):
+            raise ValidationError("Invalid password")
+
+        if self.cleaned_data["password"] != self.cleaned_data["c_password"]:
+            raise ValidationError("Passwords Must Match")
+
+        self.cleaned_data.pop("c_password")
+        return self.cleaned_data
+
+    def save(self, *args, **kwargs):
+        self.instance.set_password(self.cleaned_data["password"])
+        self.instance.save()
+
+        return self.instance
+
+
+class AccountUpdateForm(forms.ModelForm):
+
+    class Meta:
+        model = User
+        fields = [
+            "email",
             "first_name",
             "last_name",
             "institution"
         ]
+
+    def clean_email(self):
+        user = self.request.user
+
+        email = self.cleaned_data["email"]
+
+        users = User.objects.filter(email__iexact=email).exclude(
+            username__iexact=user.username)
+        if users:
+            raise forms.ValidationError(
+                "User with this email already exists.")
+
+        return email.lower()
 
     def __init__(self, request=None, *args, **kwargs) -> None:
         self.request = request
@@ -133,27 +183,18 @@ class AccountUpdateForm(forms.ModelForm):
                 "placeholder": "user@gmail.com"
             }
         )
-        self.fields["password"].widget = forms.PasswordInput(
-            attrs={
-                "class": "w-full px-3 py-2 text-sm leading-tight text-gray-700 border rounded shadow appearance-none focus:border-indigo-500 focus:outline-none focus:shadow-outline",
-                "placeholder": "*"*20
-            }
-        )
 
         self.fields["institution"].widget.attrs = {
             "class": "w-full px-3 py-2 mb-3 text-sm leading-tight focus:border-indigo-500 focus:outline-none bg-white text-black border rounded shadow focus:shadow-outline"
         }
 
-    def clean(self):
-        old_password = self.cleaned_data.pop("old_password")
-        if not self.request.user.check_password(old_password):
-            raise ValidationError("Invalid password")
 
-        if self.cleaned_data["password"] != self.cleaned_data["c_password"]:
-            raise ValidationError("Passwords Must Match")
-
-        self.cleaned_data.pop("c_password")
-        return self.cleaned_data
+class ChangeProfilePictureForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = [
+            "profile_picture"
+        ]
 
 
 class AccountLoginForm(forms.Form):
--- account/views.py
+++ account/views.py
@@ -1,12 +1,12 @@
 from django.urls import reverse_lazy
 from django.views import generic
 from django.contrib.auth import logout
+
 from django.shortcuts import redirect, get_object_or_404, render
 from django.conf import settings
 from django.contrib.auth import get_user_model
 from django.contrib.auth.mixins import LoginRequiredMixin
-
-from .forms import AccountCreationForm, AccountLoginForm, AccountUpdateForm
+from .forms import AccountCreationForm, AccountLoginForm, AccountUpdateForm, ChangePasswordForm, ChangeProfilePictureForm
 
 
 class AccountCreationView(generic.CreateView):
@@ -57,16 +57,67 @@ class UpdateProfileView(LoginRequiredMixin, generic.FormView):
         return initial
 
     def get_form_kwargs(self):
+
         kwargs = super().get_form_kwargs()
         kwargs["request"] = self.request
+        kwargs["instance"] = self.request.user
 
         return kwargs
 
+    def get_context_data(self, **kwargs):
+        context = super(generic.FormView, self).get_context_data()
+        context["form"] = self.form_class(
+            self.request, instance=self.request.user)
+        context["password"] = ChangePasswordForm(
+            self.request, instance=self.request.user)
+        context["profile"] = ChangeProfilePictureForm(
+            self.request, instance=self.request.user
+        )
+        return context
+
     def get(self, request):
 
-        form = self.get_form()
+        return render(request, self.template_name, context=self.get_context_data())
 
-        return render(request, self.template_name, context={"form": form})
+    def post(self, request):
+        form = None
+        print(request.POST)
+        if "user_detail" in request.POST:
+            form = self.get_form()
+
+        elif "change_password" in request.POST:
+            form = ChangePasswordForm(
+                self.request, request.POST, instance=self.request.user)
+
+        elif "profile_pic_btn" in request.POST:
+            form = ChangeProfilePictureForm(
+                files=request.FILES, data=request.POST, instance=self.request.user
+            )
+
+        if form.is_valid():
+            return self.form_valid(form)
+
+        return self.form_invalid(form)
+
+    def form_valid(self, form):
+
+        form.save()
+        if isinstance(form, ChangePasswordForm):
+            logout(request=self.request)
+            return redirect(reverse_lazy("home"))
+
+        return redirect(reverse_lazy("account-profile", kwargs={"id": self.request.user.id}))
+
+    def form_invalid(self, form):
+        context = self.get_context_data()
+        if isinstance(form, ChangePasswordForm):
+            context["password"] = form
+        elif isinstance(form, AccountUpdateForm):
+            context["form"] = form
+        elif isinstance(form, ChangeProfilePictureForm):
+            context["profile"] = form
+
+        return render(self.request, self.template_name, context=context)
 
 
 class LogoutView(LoginRequiredMixin, generic.TemplateView):
@@ -74,5 +125,5 @@ class LogoutView(LoginRequiredMixin, generic.TemplateView):
     template_name = 'logout.html'
 
     def post(self, request):
-        logout(self.request)
+        logout(request)
         return redirect(settings.LOGOUT_REDIRECT_URL)
