77e48106576a5e4cef99a3b2e478da314798250c
toluhunter <toluhunter19@gmail.com>
2023-04-15 22:48:37 +0100

    Removed debug mail

--- projectrepo/views.py
+++ projectrepo/views.py
@@ -48,7 +48,6 @@ def webhookview(request):
             return JsonResponse({"Success": False}, status=400)
 
         score, res = plagiarism.fetch_plagiarism_details(project)
-        send_mail(subject='final check', message=json.dumps(res), from_email=settings.EMAIL_HOST_USER, recipient_list=["toluhunter19@gmail.com"])
 
         if score >= 25:
             project.status = "failed"
