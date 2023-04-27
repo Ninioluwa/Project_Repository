f8128fec69d363064d6bee359d3d610be48c7423
toluhunter <toluhunter19@gmail.com>
2023-04-27 11:08:22 +0100

    Fixed active filter true

--- account/views.py
+++ account/views.py
@@ -43,7 +43,8 @@ class ProfileView(LoginRequiredMixin, generic.DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context["projects"] = self.request.user.projects.filter(active=True)
+        user = self.get_object()
+        context["projects"] = user.projects.filter(active=True)
 
         return context
 
